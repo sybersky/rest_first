5da503bf8c8f225a6fe94d961ee097d3d967d13c
sybersky <60616@mail.ru>
2020-07-28 14:54:00 +0300

    add saraphan as first rest

---
+++ src/main/java/com/sybersky/saraphan/SaraphanNameApplication.java
@@ -0,0 +1,13 @@
+package com.sybersky.saraphan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraphanNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaraphanNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sybersky/saraphan/SaraphanNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sybersky.saraphan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaraphanNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
